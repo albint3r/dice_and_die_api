95fbafeb89f7f9977a8500a92728d097bb577b05
albint3r <albint3r@gmail.com>
2023-11-19 14:46:38 -0600

    Add player test

--- src/test/test_player.py
+++ src/test/test_player.py
@@ -2,6 +2,8 @@ import pytest
 
 from src.domain.board.board import Board
 from src.domain.die.die import Die
+from src.domain.player.errors import InvalidNewBoard, InvalidNewDie
+from src.domain.player.player import Player
 
 
 class TestPlayer:
@@ -15,5 +17,25 @@ class TestPlayer:
     def board(self):
         return Board()
 
-    # def test_
+    def test_invalid_new_board(self):
+        """Test invalid new board. Expected an empty board columns"""
+        die = Die()
+        board = Board()
+        board.add(1, 1)
+        with pytest.raises(InvalidNewBoard):
+            _ = Player(die=die, board=board)
 
+    def test_invalid_new_die(self):
+        """Test invalid new die. Expected a None number die value"""
+        die = Die()
+        die.roll()
+        board = Board()
+        with pytest.raises(InvalidNewDie):
+            _ = Player(die=die, board=board)
+
+    def test_valid_new_player(self, board, die):
+        """Test correct player creations"""
+        expected = "Player()"
+        result = Player(board=board, die=die)
+        error_msg = f'Expected value: {expected}. Result: {result}'
+        assert isinstance(result, Player), error_msg

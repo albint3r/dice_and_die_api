ba05479e083bf86634b511d27fb982184edaeb41
albint3r <albint3r@gmail.com>
2023-12-09 17:53:58 -0600

    Add creation SQL in docker

---
+++ src/db/1_init.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE IF NOT EXISTS dice_and_die;
+USE dice_and_die;
+
+CREATE TABLE users (
+    user_id VARCHAR(36) DEFAULT (UUID()) PRIMARY KEY UNIQUE,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(20) NOT NULL,
+    is_verify BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE ranks (
+    rank_id BIGINT PRIMARY KEY AUTO_INCREMENT UNIQUE,
+    name VARCHAR(30) NOT NULL UNIQUE
+);
+
+CREATE TABLE users_level (
+    user_level VARCHAR(36) DEFAULT (UUID()) PRIMARY KEY UNIQUE,
+    user_id CHAR(36),
+    rank_id BIGINT,
+    level INTEGER DEFAULT 0,
+    current_points INTEGER DEFAULT 0,
+    next_level_points INTEGER DEFAULT 0,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (rank_id) REFERENCES ranks(rank_id)
+);
+
+CREATE TABLE bank_accounts (
+    bank_accounts VARCHAR(36) DEFAULT (UUID()) PRIMARY KEY UNIQUE,
+    user_id VARCHAR(36) NOT NULL UNIQUE,
+    amount DOUBLE NOT NULL DEFAULT 0
+);

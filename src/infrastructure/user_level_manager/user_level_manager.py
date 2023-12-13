0216bce63b8b5058d24a8bda3c143a1e2616b06a
albint3r <albint3r@gmail.com>
2023-12-12 23:19:25 -0600

    Check user level manager works

--- src/infrastructure/user_level_manager/user_level_manager.py
+++ src/infrastructure/user_level_manager/user_level_manager.py
@@ -1,21 +1,39 @@
+from typing import Callable
+
 from pydantic import BaseModel, validate_call
 
 from src.domain.auth.user_level import UserLevel
 
 
+def next_level_basic_formula(user_level: UserLevel) -> int:
+    level = user_level.level
+    exponent1 = 0.1
+    exponent2 = 0.8
+    base_exp = 50
+    return int(exponent1 * (level ** 3) + exponent2 * (level ** 2) + base_exp * level)
+
+
+def next_level_advance_formula(user_level: UserLevel) -> int:
+    exponent = 1.5
+    base_exp = 100
+    return int(base_exp * (user_level.level ** exponent))
+
+
 class UserLevelManager(BaseModel):
-    exponent: float = 1.5
-    base_exp: int = 1000
 
     @validate_call()
-    def next_level(self, user_level: UserLevel) -> int:
+    def next_level(self, user_level: UserLevel, *, formula: Callable[[UserLevel], int]) -> int:
         """Get How Many Point Need for the next level"""
-        return int(self.base_exp * (user_level.level ** self.exponent))
+        return formula(user_level)
 
     @validate_call()
-    def ready_to_level_up(self, user_level: UserLevel) -> bool:
+    def ready_to_level_up(self, user_level: UserLevel, *,
+                          formula: Callable[[UserLevel], int] = next_level_basic_formula) -> bool:
         """Check if the user have equal or more point to upgrade to the next level"""
+        points_to_next_lvl = formula(user_level)
+        return user_level.current_points >= points_to_next_lvl
 
     @validate_call()
     def add_exp_points(self, user_level: UserLevel, exp_points: int) -> int:
         """Add experience point to the user level"""
+        return user_level.current_points + exp_points
--- src/test/test_user_level_manager.py
+++ src/test/test_user_level_manager.py
@@ -6,7 +6,8 @@ from src.db.db import _DataBase
 from src.domain.auth.i_auth_facade import IAuthFacade
 from src.domain.auth.user import User
 from src.infrastructure.auth.auth_facade_impl import AuthFacadeImpl
-from src.infrastructure.user_level_manager.user_level_manager import UserLevelManager
+from src.infrastructure.user_level_manager.user_level_manager import UserLevelManager, next_level_basic_formula, \
+    next_level_advance_formula
 from src.repositories.auth.auth_handler_impl import auth_handler, _AuthHandlerImpl
 from src.repositories.auth.auth_repository import AuthRepository
 
@@ -37,11 +38,103 @@ class TestUserLevelManager:
     def user_level_manager_impl(self) -> UserLevelManager:
         return UserLevelManager()
 
-    def test_fake_user_exist(self, fake_user_1, user_level_manager_impl):
+    def test_next_level_formulas(self, fake_user_1, user_level_manager_impl):
         """Validate the fake User creation"""
         error_msg = f"Expected [User]. Result: [{fake_user_1}]"
         assert isinstance(fake_user_1, User), error_msg
-        # Check next point for level up:
-        fake_user_1.user_level.level = 2
-        ic(user_level_manager_impl.next_level(fake_user_1.user_level))
-        assert False
+        # Check basic formula
+        expected = 50
+        result = user_level_manager_impl.next_level(fake_user_1.user_level, formula=next_level_basic_formula)
+        error_msg = f"Expected: {expected}. Result: {result}"
+        assert expected == result, error_msg
+        # Check Advance formula
+        expected = 100
+        result = user_level_manager_impl.next_level(fake_user_1.user_level, formula=next_level_advance_formula)
+        error_msg = f"Expected: {expected}. Result: {result}"
+        assert expected == result, error_msg
+
+    def test_add_exp_points_not_save_level(self, fake_user_1, user_level_manager_impl):
+        """Validate the fake User creation"""
+        # Check basic formula
+        exp_points = 50
+        result = user_level_manager_impl.add_exp_points(fake_user_1.user_level, exp_points)
+        expected = 51
+        error_msg = f"1) Expected: {expected}. Result: {result}"
+        assert expected == result, error_msg
+        # Second test after add first points
+        exp_points = 20
+        result = user_level_manager_impl.add_exp_points(fake_user_1.user_level, exp_points)
+        expected = 21
+        error_msg = f"2) Expected: {expected}. Result: {result}"
+        assert expected == result, error_msg
+        # Third test after add the second points
+        exp_points = 35  # Total 105
+        result = user_level_manager_impl.add_exp_points(fake_user_1.user_level, exp_points)
+        expected = 36
+        error_msg = f"3) Expected: {expected}. Result: {result}"
+        assert expected == result, error_msg
+
+    def test_add_exp_points_yes_save_level(self, fake_user_1, user_level_manager_impl):
+        """Validate the fake User creation"""
+        # Check basic formula
+        exp_points = 50
+        fake_user_1.user_level.current_points = user_level_manager_impl.add_exp_points(fake_user_1.user_level,
+                                                                                       exp_points)
+        result = fake_user_1.user_level.current_points
+        expected = 51
+        error_msg = f"1) Expected: {expected}. Result: {result}"
+        assert expected == result, error_msg
+        # Second test after add first points
+        exp_points = 20
+        fake_user_1.user_level.current_points = user_level_manager_impl.add_exp_points(fake_user_1.user_level,
+                                                                                       exp_points)
+        result = fake_user_1.user_level.current_points
+        expected = 71
+        error_msg = f"2) Expected: {expected}. Result: {result}"
+        assert expected == result, error_msg
+        # Third test after add the second points
+        exp_points = 35  # Total 105
+        fake_user_1.user_level.current_points = user_level_manager_impl.add_exp_points(fake_user_1.user_level,
+                                                                                       exp_points)
+        result = fake_user_1.user_level.current_points
+        expected = 106
+        error_msg = f"3) Expected: {expected}. Result: {result}"
+        assert expected == result, error_msg
+
+    def test_ready_to_level_up_advance_formula(self, fake_user_1, user_level_manager_impl):
+        """Validate the fake User creation"""
+        # Check basic formula
+        exp_points = 50
+        fake_user_1.user_level.current_points = user_level_manager_impl.add_exp_points(fake_user_1.user_level,
+                                                                                       exp_points)
+        result = user_level_manager_impl.ready_to_level_up(fake_user_1.user_level, formula=next_level_advance_formula)
+        expected = False
+        error_msg = f"1) Expected: {expected}. Result: {result}"
+        assert expected is result, error_msg
+        # User Pass the first threshold and have enough point to upgrade
+        exp_points = 55
+        fake_user_1.user_level.current_points = user_level_manager_impl.add_exp_points(fake_user_1.user_level,
+                                                                                       exp_points)
+        result = user_level_manager_impl.ready_to_level_up(fake_user_1.user_level, formula=next_level_advance_formula)
+        expected = True
+        error_msg = f"1) Expected: {expected}. Result: {result}"
+        assert expected is result, error_msg
+
+    def test_ready_to_level_up_basic_formula(self, fake_user_1, user_level_manager_impl):
+        """Validate the fake User creation"""
+        # Check basic formula
+        exp_points = 30
+        fake_user_1.user_level.current_points = user_level_manager_impl.add_exp_points(fake_user_1.user_level,
+                                                                                       exp_points)
+        result = user_level_manager_impl.ready_to_level_up(fake_user_1.user_level, formula=next_level_basic_formula)
+        expected = False
+        error_msg = f"1) Expected: {expected}. Result: {result}"
+        assert expected is result, error_msg
+        # User Pass the first threshold and have enough point to upgrade
+        exp_points = 40
+        fake_user_1.user_level.current_points = user_level_manager_impl.add_exp_points(fake_user_1.user_level,
+                                                                                       exp_points)
+        result = user_level_manager_impl.ready_to_level_up(fake_user_1.user_level, formula=next_level_basic_formula)
+        expected = True
+        error_msg = f"1) Expected: {expected}. Result: {result}"
+        assert expected is result, error_msg

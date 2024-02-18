5d7359c79b60d3e2a99e31c0c56ac4bc4893b310
albint3r <albint3r@gmail.com>
2024-02-17 18:37:23 -0600

    Add Destroy opponent target column case

--- app/infrastructure/game/game_use_case.py
+++ app/infrastructure/game/game_use_case.py
@@ -5,6 +5,7 @@ from starlette.websockets import WebSocket
 from app.domain.auth.entities.user import User
 from app.domain.core.ref_types import TGamePlayer
 from app.domain.game.entities.board import Board
+from app.domain.game.entities.column import Column
 from app.domain.game.entities.die import Die
 from app.domain.game.entities.game import Game
 from app.domain.game.entities.player import Player
@@ -56,23 +57,36 @@ class GameUseCase(IGameUseCase):
                                                        message=f'User {game.current_player} roll dice',
                                                        extras={})
             case GameState.SELECT_COLUMN:
-                request = kwargs['selected_column']
-                column_index = self._select_column(request)
+                request: GamePlayerRequest = kwargs['selected_column']
+                column_index = self._select_column(request.event.value)
                 column = game.current_player.board.columns.get(column_index)
                 if column_index and not column.is_full():
-                    die_val = game.current_player.die.current_number
-                    column.add(die_val)
-                    opponent_player = game.get_opponent_player()
-                    column_opponent_player = opponent_player.board.columns.get(column_index)
                     game.state = GameState.ADD_DICE_TO_COLUMN
+                    await self.execute(game, column_index=column_index)
+
+            case GameState.ADD_DICE_TO_COLUMN:
+                column_index: int = kwargs['column_index']
+                column = game.current_player.board.columns.get(column_index)
+                die_val = game.current_player.die.current_number
+                column.add(die_val)
+                opponent_player = game.get_opponent_player()
+                column_opponent_player = opponent_player.board.columns.get(column_index)
+                if column_opponent_player.can_remove_values(die_val):
+                    game.state = GameState.DESTROY_OPPONENT_TARGET_COLUMN
+                    await self.execute(game, column_opponent_player=column_opponent_player, die_val=die_val)
+                else:
+                    game.state = GameState.UPDATE_PLAYERS_POINTS
                     await self.websocket_manager.broadcast(game_id=game.game_id,
                                                            message='add_dice_to_colum',
                                                            extras={})
-                    if column_opponent_player.can_remove_values(die_val):
-                        removed_indices = column.remove(die_val)
-                        await self.websocket_manager.broadcast(game_id=game.game_id,
-                                                               message='destroy_opponent_target_column',
-                                                               extras={'removed_indices': removed_indices})
+            case GameState.DESTROY_OPPONENT_TARGET_COLUMN:
+                column_opponent_player: Column = kwargs['column_opponent_player']
+                die_val: int = kwargs['die_val']
+                removed_indices = column_opponent_player.remove(die_val)
+                game.state = GameState.UPDATE_PLAYERS_POINTS
+                await self.websocket_manager.broadcast(game_id=game.game_id,
+                                                       message='destroy_opponent_target_column',
+                                                       extras={'removed_indices': removed_indices})
 
     async def create_or_join_game(self, game_id: str, user_id: str, websocket: WebSocket) -> TGamePlayer:
         game = self.websocket_manager.active_games.get(game_id)

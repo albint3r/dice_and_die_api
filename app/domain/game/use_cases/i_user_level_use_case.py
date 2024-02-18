f093501c7eed9d9f4ea3375107bbeb78dc7b5fd2
albint3r <albint3r@gmail.com>
2024-02-18 17:38:21 -0600

    Add leveling tool

--- app/domain/game/use_cases/i_user_level_use_case.py
+++ app/domain/game/use_cases/i_user_level_use_case.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 
 from pydantic import BaseModel
 
+from app.domain.game.use_cases.i_leveling_use_case import ILevelUseCase, IRankUseCase
 from src.domain.auth.user import User
 from src.domain.core.i_game_manager import IGameManager
 from src.domain.game.game import Game
@@ -15,6 +16,13 @@ class IManagerLevelingUseCase(BaseModel, ABC):
 
     @abstractmethod
     def update_user_level(self, user: User, exp_points: int,
-                          leve_manager: IGameManager,
-                          rank_manager: IGameManager) -> User:
-        """Update the user experience points"""
+                          leve_manager: ILevelUseCase,
+                          rank_manager: IRankUseCase) -> User:
+        """Update User Level after win.
+        In this facade the user level will be updated on:
+        - Xp points
+        - Level
+        - Rank
+
+        If any of these conditions apply it will the user entity and returned.
+        """
--- app/infrastructure/game/manager_leveling_use_case.py
+++ app/infrastructure/game/manager_leveling_use_case.py
@@ -1,5 +1,6 @@
-from app.domain.game.use_cases.i_leveling_use_case import ILeveling
+from app.domain.game.use_cases.i_leveling_use_case import ILeveling, IRankUseCase, ILevelUseCase
 from app.domain.game.use_cases.i_user_level_use_case import IManagerLevelingUseCase
+from app.infrastructure.game.formulas import next_level_basic_formula
 from src.domain.auth.user import User
 from src.domain.game.game import Game
 
@@ -13,6 +14,29 @@ class ManagerLevelingUseCase(IManagerLevelingUseCase):
     def update_user_level(self,
                           user: User,
                           exp_points: int,
-                          leve_manager: ILeveling,
-                          rank_manager: ILeveling) -> User:
-        """pass"""
+                          leve_manager: ILevelUseCase,
+                          rank_manager: IRankUseCase) -> User:
+        """Update User Level after win.
+        In this facade the user level will be updated on:
+        - Xp points
+        - Level
+        - Rank
+
+        If any of these conditions apply it will the user entity and returned.
+        """
+        user.user_level.exp_points = leve_manager.add_exp_points(user.user_level, exp_points)
+        ready_to_level_up = leve_manager.ready_to_progress(user_level=user.user_level, formula=next_level_basic_formula)
+        if ready_to_level_up:
+            # Before update the user level We need to get the difference of the remaining points
+            # Example:
+            # Level to need 50 exp
+            # You have 40 + 30 = 70
+            # You have a difference of 20 point for the level to block.
+            user.user_level.exp_points = user.user_level.exp_points - user.user_level.next_level_points
+            user.user_level = leve_manager.progress(user_level=user.user_level, formula=next_level_basic_formula)
+            # User can upgrade rank?
+            ready_to_rank_up = rank_manager.ready_to_progress(user_level=user.user_level)
+            if ready_to_rank_up:
+                user.user_level = rank_manager.ready_to_progress(user.user_level)
+
+        return user

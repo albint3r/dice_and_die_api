fb47740d5e399ac66869f5de94b9321655af71dc
albint3r <albint3r@gmail.com>
2024-03-29 16:26:12 -0600

    Add creation users

--- app/infrastructure/game/pve_game_use_case.py
+++ app/infrastructure/game/pve_game_use_case.py
@@ -5,30 +5,40 @@ from app.domain.game.entities.board import Board
 from app.domain.game.entities.die import Die
 from app.domain.game.entities.game import Game
 from app.domain.game.entities.player import Player
+from app.domain.game.entities.player_rol import PlayerRol
 from app.domain.game.enums.game_state import GameState
 from app.domain.game.schemas.request import GamePlayerRequest
 from app.domain.game.use_cases.i_game_use_case import IGameUseCase
 from app.domain.game.use_cases.i_game_websocket_manager import IGameWebSocketManager
 from app.repositories.auth.auth_repository import AuthRepository
+from app.domain.auth.entities.user import User
 
 
 class PVEGameUseCase(IGameUseCase):
     websocket_manager: IGameWebSocketManager
     repo: AuthRepository
 
+    def _get_user(self, user_id: str) -> User:
+        """Create an instance of the user by the user id"""
+        user = self.repo.get_user_by_id(user_id)
+        user.user_level = self.repo.get_user_level(user.user_id)
+        user.bank_account = self.repo.get_user_bank_account(user.user_id)
+        return user
+
     def _create_new_player(self, user: User) -> Player:  # noqa
         """Create a new player from an existing user."""
         return Player(user=user, board=Board(), die=Die())
 
-
-    def _create_new_game(self, game_id: str, player: Player) -> Game:  # noqa
+    def _create_new_game(self, game_id: str, player: Player, player_ai: Player) -> Game:  # noqa
         """Create a new Game."""
-        return Game(game_id=game_id, p1=player, state=GameState.CREATE_NEW_GAME)
+        return Game(game_id=game_id, p1=player, p2=player_ai, state=GameState.CREATE_NEW_GAME)
 
     async def create_or_join(self, game_id: str, user_id: str, websocket: WebSocket) -> TGamePlayer:
         """Create a room for the player and an AI."""
         player = self._create_new_player(self._get_user(user_id))
-        game = self._create_new_game(game_id, player)
+        player_ai = self._create_new_player(self._get_user('I007'))
+        player_ai.rol = PlayerRol.AI
+        game = self._create_new_game(game_id, player, player_ai)
         await self.websocket_manager.connect(game_id=game_id, new_game=game, websocket=websocket)
 
     async def execute(self, game: Game):

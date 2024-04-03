0354ce271f16b3ddb3b5e43fe409fe2c051ff334
albint3r <albint3r@gmail.com>
2024-04-03 16:23:00 -0600

    Add inyectables

--- app/inyectables.py
+++ app/inyectables.py
@@ -5,33 +5,79 @@ from fastapi import Depends
 from pydantic import BaseModel
 
 from app.db.db import db
+from app.domain.analytics.use_cases.i_analytics_use_case import IAnalyticsUseCase
 from app.domain.auth.use_cases.i_auth_use_case import IAuthUseCase
+from app.domain.lobby.use_cases.i_lobby_use_case import ILobbyUseCase
+from app.domain.lobby.use_cases.i_lobby_websocket_manager import ILobbyWebSocketManager
+from app.infrastructure.analytics.analytics_use_case import AnalyticsUseCase
 from app.infrastructure.auth.auth_use_case import AuthUseCase
+from app.infrastructure.game.game_websocket_manager import game_websocket_manger
+from app.infrastructure.lobby.lobby_use_case import LobbyUseCase
+from app.infrastructure.lobby.lobby_websocket_manager import lobby_websocket_manager
+from app.repositories.analytics.analytics_repository import AnalyticsRepository
 from app.repositories.auth.auth_repository import AuthRepository
 
-T = TypeVar('T')
 
-
-class Inyectables(BaseModel, ABC):
+class Inyectables(ABC):
     @staticmethod
     @abstractmethod
-    def get(*args, **kwargs):
+    def inject(*args, **kwargs):
         """The user the inyectable function"""
 
 
-class AuthDependency(Inyectables):
+class AuthRepositoryDependency(Inyectables):
     @staticmethod
-    def get() -> AuthRepository:
+    def inject() -> AuthRepository:
         return AuthRepository(db=db)
 
 
-auth_repository_depend = Annotated[AuthRepository, Depends(AuthDependency.get)]
+auth_repository_depend = Annotated[AuthRepository, Depends(AuthRepositoryDependency.inject)]
+
+
+class AnalyticsRepositoryDependency(Inyectables):
+    @staticmethod
+    def inject() -> AnalyticsRepository:
+        return AnalyticsRepository(db=db)
+
+
+analytics_repository_depend = Annotated[AnalyticsRepository, Depends(AnalyticsRepositoryDependency.inject)]
 
 
 class AuthUseCaseDependency(Inyectables):
     @staticmethod
-    def get(repo: auth_repository_depend) -> IAuthUseCase:
+    def inject(repo: auth_repository_depend) -> IAuthUseCase:
         return AuthUseCase(repo=repo)
 
 
-auth_use_case_depend = Annotated[IAuthUseCase, Depends(AuthUseCaseDependency.get)]
+auth_use_case_depend = Annotated[IAuthUseCase, Depends(AuthUseCaseDependency.inject)]
+
+
+class AnalyticUseCaseDependency(Inyectables):
+
+    @staticmethod
+    def inject(repo: analytics_repository_depend) -> IAnalyticsUseCase:
+        return AnalyticsUseCase(repo=repo)
+
+
+analytics_use_case_depend = Annotated[IAnalyticsUseCase, Depends(AnalyticUseCaseDependency.inject)]
+
+
+class LobbyWebSocketDependency(Inyectables):
+
+    @staticmethod
+    def inject(*args, **kwargs) -> ILobbyWebSocketManager:
+        return lobby_websocket_manager
+
+
+lobby_websocket_dependency = Annotated[ILobbyWebSocketManager, Depends(LobbyWebSocketDependency.inject)]
+
+
+class LobbyUseCaseDependency(Inyectables):
+
+    @staticmethod
+    def inject(*args, **kwargs) -> ILobbyUseCase:
+        return LobbyUseCase(lobby_websocket_manager=lobby_websocket_manager,
+                            game_websocket_manager=game_websocket_manger)
+
+
+lobby_use_case_dependency = Annotated[ILobbyUseCase, Depends(LobbyUseCaseDependency.inject)]
--- app/routes/analytics/analytics.py
+++ app/routes/analytics/analytics.py
@@ -1,23 +1,11 @@
-from typing import Annotated
+from fastapi import APIRouter
 
-from fastapi import APIRouter, Depends
-
-from app.db.db import db
 from app.domain.analytics.entities.single_play_history import SinglePlayHistory
-from app.domain.analytics.use_cases.i_analytics_use_case import IAnalyticsUseCase
-from app.infrastructure.analytics.analytics_use_case import AnalyticsUseCase
-from app.repositories.analytics.analytics_repository import AnalyticsRepository
+from app.inyectables import analytics_use_case_depend
 
 router = APIRouter(tags=['analytics'], prefix='/v2/analytics')
 
 
-async def _get_use_case() -> IAnalyticsUseCase:
-    return AnalyticsUseCase(repo=AnalyticsRepository(db=db))
-
-
-analytics_dependency = Annotated[IAnalyticsUseCase, Depends(_get_use_case)]
-
-
 @router.get('/play/history')
-async def get_single_play_history(use_case: analytics_dependency) -> list[SinglePlayHistory]:
+async def get_single_play_history(use_case: analytics_use_case_depend) -> list[SinglePlayHistory]:
     return use_case.get_plays_history()
